a137351ed96f2eda394e392ba5b43b512646c6e1
Abhelaasha <113101675+Abhelaasha@users.noreply.github.com>
2022-12-10 00:16:30 +0530

    Delete L3_Abhelaasha_V_2022H1540833P.py

--- L3_Abhelaasha_V_2022H1540833P.py
+++
@@ -1,7 +0,0 @@
-import httpimport #importing the package httpimport
-url = "https://raw.githubusercontent.com/Abhelaasha/PFA_Assignment/main/" #github url for the corresponding hosted python module
-with httpimport.remote_repo(url): #importing the repository for the given url
-    import factorial as p #importing the necessary package into the program or application
-    import circle as c #importing the necessray package into the programme
-print("Given number ",4," ",p.fact(4))
-print("Given radius ",7," ",c.area(7))
